4a06446cc2378dff599b26b913f88150d5653a3c
tomascosta03 <tomascosta.3@hotmail.com>
2024-05-04 17:21:24 +0000

    Connecting to DB

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,28 @@
 import express from "express";
 import morgan from "morgan";
 import dotenv from "dotenv";
-import UserRouter from "./user/routes";
+import UserRouter from "./routes/user/routes";
+
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
+const mongoose = require('mongoose');
 
+app.use(express.json())
 app.use(morgan('tiny'));
 app.use("/user", UserRouter);
 
-app.listen(port, () => {
-    console.log(`The server is listening on: http://localhost:${port}`);
-});
+
+// ligar a bd
+mongoose.connect(process.env.DB)
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Database connected successfully`);
+            console.log(`The server is listening on: http://localhost:${port}`);
+        });
+    })
+    .catch((error: any) =>{
+        console.log(error)
+    })
