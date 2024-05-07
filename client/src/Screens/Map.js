3f48f8cac162e56c3f02ef54ea391e4a2d21066a
Feropes <tiagofanfa8@gmail.com>
2024-05-07 13:27:08 +0000

    Parque funcional com informações a aparecer no Market Popup! Falta agora sincronizar com a base de dados os lugares disponiveis. E adicionar mais parques. Estilizar também o popup para ser mais compativel com a mockup do Figma.

--- client/src/Screens/Map.js
+++ client/src/Screens/Map.js
@@ -1,22 +1,53 @@
-import * as React from 'react';
-import Map, { Marker } from 'react-map-gl';
+import React, { useState } from 'react';
+import Map, { Marker, Popup } from 'react-map-gl';
 
 function App() {
-  return <Map
-    mapboxAccessToken="pk.eyJ1IjoiZmVyb3BlcyIsImEiOiJjbHZ0amdxa3kxYnp3MmxtaGo3bjkzcml3In0.IVlmneQW7Me9kUfvoF6DkQ"
-    initialViewState={{
-      longitude: -25.6661979,
-      latitude: 37.7459205,
-      zoom: 16
-    }}
-    style={{width: "100vw", height: "100vh"}}
-    mapStyle="mapbox://styles/mapbox/streets-v9"
-  >
-    <Marker longitude={-25.6661979} latitude={37.7459205} anchor="bottom" >
-      <img width={35} height={35} src="https://w7.pngwing.com/pngs/258/471/png-transparent-car-park-parking-escalator-blue-electronics-text-thumbnail.png" />
-    </Marker>
-  </Map>;
-}
+  const [showInfo, setShowInfo] = useState(false);
 
+  return (
+    <Map
+      mapboxAccessToken="pk.eyJ1IjoiZmVyb3BlcyIsImEiOiJjbHZ0amdxa3kxYnp3MmxtaGo3bjkzcml3In0.IVlmneQW7Me9kUfvoF6DkQ"
+      initialViewState={{
+        longitude: -25.6661979,
+        latitude: 37.7459205,
+        zoom: 16
+      }}
+      style={{width: "100vw", height: "100vh"}}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+    >
+      <Marker
+        longitude={-25.6661979}
+        latitude={37.7459205}
+        anchor="bottom"
+        onClick={() => setShowInfo(true)}
+      >
+        {showInfo && (
+          <Popup
+            longitude={-25.6661979}
+            latitude={37.7459205}
+            closeButton={true}
+            closeOnClick={false}
+            onClose={() => setShowInfo(false)}
+            anchor="top"
+          >
+            <div>
+              <h2>Casa do Tomás</h2>
+              <p>Rua secreta!</p>
+              <p>Lugares Disponíveis: 1 para o Fanfa</p>
+              <p>Lugares Ocupados: 1 o Tomás</p>
+            </div>
+          </Popup>
+        )}
+        <img
+          width={35}
+          height={35}
+          src="https://w7.pngwing.com/pngs/258/471/png-transparent-car-park-parking-escalator-blue-electronics-text-thumbnail.png"
+          alt="Parque"
+          style={{ cursor: 'pointer' }}
+        />
+      </Marker>
+    </Map>
+  );
+}
 
 export default App;
